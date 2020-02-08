f0e74b639479a68a7e7f4d8b88fc72372cd6e401
Victor <v.chumakovv@gmail.com>
2020-02-08 22:38:11 +0200

    homework 2 complite

--- week-01-hw2/week-01-02.js
+++ week-01-hw2/week-01-02.js
@@ -17,8 +17,63 @@ if ((intNumberN ^ 0) === intNumberN){
 
 //3
 let numberMAnswer = prompt("write M");
-let intNumberAnswer = parseInt(numberMAnswer);
-console.log(intNumberAnswer);
+let intNumberM = parseInt(numberMAnswer);
+console.log(intNumberM);
 
 //4
+if ((intNumberM ^ 0) === intNumberM){
+    console.log(true);
+} else {
+    console.log(false);
+};
+
+//5
+let confirmNumber = confirm("dont need even int?");
+
+
+let sumA = 0;
+for (let i = intNumberN; i <= intNumberM; i++) {
+    if (confirmNumber = true) {
+        if (i % 2 == 0) {
+            console.log("fuck", i)
+        } else {
+            console.log("this even +", i)
+            sumA += i;
+
+        };
+
+    } else {
+        console.log("false answer");
+        sumA += i;
+    };
+
+
+  }
+  console.log("m", sumA);
+
+
+
+
+  // let sumB = 0;
+// for (let m = 1; m <= intNumberN; m++) {
+//     if (confirmNumber = true) {
+//         if (m % 2 == 0) {
+//         } else {
+//             sumB += m;
+//         }
+
+//     } else {
+//         sumB += m;
+//     }
+
+
+//   }
+
+//   let allSum = sumA + sumB;
+//   console.log("full sum", allSum);
+
+  
+
+
+
 
