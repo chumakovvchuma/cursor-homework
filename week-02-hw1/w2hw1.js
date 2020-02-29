4ef09b18e91f7726818d36328399b835c03ec853
Victor <v.chumakovv@gmail.com>
2020-02-29 23:14:42 +0200

    fix 3

--- week-02-hw1/w2hw1.js
+++ week-02-hw1/w2hw1.js
@@ -1,93 +1,93 @@
-// //4
-
-
-// let wage = 1000;
-// let tax = 18;
-
-// const taxOnMyWage = function(wage, tax) {
-//   const dapTax = 1.5;
-//   while (isNaN(wage)) {
-//     wage = parseInt(prompt("Your wage"), 10);
-//   }
-//   while (isNaN(tax)) {
-//     tax = parseInt(prompt("tax your wage"), 10);
-//   }
-
-//     const fullTax = tax + dapTax;
-//     const taxWage = wage / 100 * fullTax;
-//     const result = wage - taxWage; 
-//     const res = result.toFixed();
-//     return res;
-// }
-// document.writeln(`wage out tax - ${taxOnMyWage()} <br>`);
 
 
 
-// //8
-// let numLength = 2;
-// const getRandomPassword = function(numLength){
-//     let pass = "";
+let wage = 1000;
+let tax = 18;
 
-//     while (isNaN(numLength)) {
-//       numLength = parseInt(prompt("Length your password"), 10);
-//     }
+const taxOnMyWage = function(wage, tax) {
+  const dapTax = 1.5;
+  while (isNaN(wage)) {
+    wage = parseInt(prompt("Your wage"), 10);
+  }
+  while (isNaN(tax)) {
+    tax = parseInt(prompt("tax your wage"), 10);
+  }
 
-//     for (let i = 1; i <= numLength; i++) {
-//         const x = Math.floor((Math.random() * 9) + 1);
-//         pass += x;
-//     }
-//     return pass;
-//   }
-// document.writeln(`your pass - ${getRandomPassword()} <br>`);
-
-
-
-
-// let min = 1;
-// let max = 9;
-// const randomMinMax = (min, max) => {
-//   while (isNaN(min)) {
-//     min = parseInt(prompt("write num min(for rand)"), 10);
-//   }
-//   while (isNaN(max)) {
-//     max = parseInt(prompt("write num max(for rand)"), 10);
-//   }
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-// document.writeln(`min-max random - ${randomMinMax()} <br>`);
+    const fullTax = tax + dapTax;
+    const taxWage = wage / 100 * fullTax;
+    const result = wage - taxWage; 
+    const res = result.toFixed();
+    return res;
+}
+document.writeln(`wage out tax - ${taxOnMyWage()} <br>`);
 
 
-// let nameUpperCase = "";
-// const nameUpper = (nameUpperCase) => {
-// nameUpperCase = prompt("your name with up first latter");
-// let arrName = nameUpperCase.split("");
-// let completeName = "";
-// const bigWord = arrName[0];
-// completeName += bigWord.toUpperCase();
-// for (let index = 1; index < arrName.length; index++) {
-//   let word = arrName[index].toLowerCase();
-//   completeName += word;
-// }
-// return completeName;
-// }
-// document.writeln(`Your name - ${nameUpper()} <br>`);
 
-// let words = "";
-// let deleteWord = "";
-// const deleteWords = (words, deleteWord) => {
-// words = prompt("word for delete letter");
-// deleteWord = prompt("letter");
-// let arrNameWords = words.split("");
-// let completeNameWords = "";
-// for (let index = 0; index < arrNameWords.length; index++) {
-//   let wordDelete = arrNameWords[index];
-//   if (deleteWord != wordDelete) {
-//      completeNameWords += wordDelete;
-//   }
-// }
-// return completeNameWords;
-// }
-// document.writeln(`delete letter in word - ${deleteWords()} <br>`);
+//8
+let numLength = 2;
+const getRandomPassword = function(numLength){
+    let pass = "";
+
+    while (isNaN(numLength)) {
+      numLength = parseInt(prompt("Length your password"), 10);
+    }
+
+    for (let i = 1; i <= numLength; i++) {
+        const x = Math.floor((Math.random() * 9) + 1);
+        pass += x;
+    }
+    return pass;
+  }
+document.writeln(`your pass - ${getRandomPassword()} <br>`);
+
+
+
+
+let min = 1;
+let max = 9;
+const randomMinMax = (min, max) => {
+  while (isNaN(min)) {
+    min = parseInt(prompt("write num min(for rand)"), 10);
+  }
+  while (isNaN(max)) {
+    max = parseInt(prompt("write num max(for rand)"), 10);
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+document.writeln(`min-max random - ${randomMinMax()} <br>`);
+
+
+let nameUpperCase = "";
+const nameUpper = (nameUpperCase) => {
+nameUpperCase = prompt("your name with up first latter");
+let arrName = nameUpperCase.split("");
+let completeName = "";
+const bigWord = arrName[0];
+completeName += bigWord.toUpperCase();
+for (let index = 1; index < arrName.length; index++) {
+  let word = arrName[index].toLowerCase();
+  completeName += word;
+}
+return completeName;
+}
+document.writeln(`Your name - ${nameUpper()} <br>`);
+
+let words = "";
+let deleteWord = "";
+const deleteWords = (words, deleteWord) => {
+words = prompt("word for delete letter");
+deleteWord = prompt("letter");
+let arrNameWords = words.split("");
+let completeNameWords = "";
+for (let index = 0; index < arrNameWords.length; index++) {
+  let wordDelete = arrNameWords[index];
+  if (deleteWord != wordDelete) {
+     completeNameWords += wordDelete;
+  }
+}
+return completeNameWords;
+}
+document.writeln(`delete letter in word - ${deleteWords()} <br>`);
 
 let wordsForCount = "";
 let countWord = "";
